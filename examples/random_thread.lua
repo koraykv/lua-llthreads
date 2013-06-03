2474b7ab27adcd77cfb8616fe5e526edc153c5b0
koray kavukcuoglu <koray@kavukcuoglu.org>
2013-06-03 23:07:52 +0100

    add nn testing

--- examples/random_thread.lua
+++ examples/random_thread.lua
@@ -20,20 +20,38 @@ function thread_func(...)
     print("CHILD: received params:", ...)
 
     -- this somehow breaks things, is it pipe or torch.PipeFile?
+    -- not really, it seems fine for linux, the problem is only the 
+    -- gnuplot package, without it, everything seems fine.
     --require 'torch-env'
     require 'torch'
+    require 'nn'
     torch.manualSeed(1)
     local mean = torch.randn(1000000):add(100):mean()
-    local ff = io.popen('ls','r')
-    local ss = ff:read('*l')
-    ff:close()
-    return mean,ss
+    local m
+    for i=1,1000 do
+       local x= torch.rand(1000)
+       local go = torch.rand(10)
+       m = nn.Sequential():add(nn.Linear(1000,100)):add(nn.Tanh()):add(nn.Linear(100,10))
+       m:forward(x)
+       m:backward(x,go)
+       collectgarbage()
+    end
+    return {mean,m.gradInput:abs():sum()}
 end
 
 require 'torch'
-
+require 'nn'
 torch.manualSeed(1)
 local expectedMean = torch.randn(1000000):add(100):mean()
+local m
+for i=1,1000 do
+   local x= torch.rand(1000)
+   local go = torch.rand(10)
+   m = nn.Sequential():add(nn.Linear(1000,100)):add(nn.Tanh()):add(nn.Linear(100,10))
+   m:forward(x)
+   m:backward(x,go)
+end
+local expectedGradInputSum = m.gradInput:abs():sum()
 
 local nChildren = 10
 local children = {}
@@ -47,12 +65,14 @@ local results = {}
 for i, child in ipairs(children) do
     local result = {child:join()}
     results[i] = result
-    print("PARENT: child returned: ", unpack(result))
+    print("PARENT: child returned: ", result[1],unpack(result[2]))
 end
 
 for i, result in ipairs(results) do
-    assert(results[i][1] == true, "expecting success")
-    assert(results[i][2] == expectedMean,
-        string.format("wrong result: %s ~= %s", results[i][2], expectedMean))
+   assert(results[i][1] == true, "expecting success")
+   assert(results[i][2][1] == expectedMean,
+	  string.format("wrong result: %s ~= %s", results[i][2][1], expectedMean))
+   assert(results[i][2][2] == expectedGradInputSum,
+	  string.format("wrong result: %s ~= %s", results[i][2][2], expectedGradInputSum))
 end
 
