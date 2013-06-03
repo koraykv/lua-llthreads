17a9f3b8274cae0617f7ca3eb7e9d10ef76604cb
koray kavukcuoglu <koray@kavukcuoglu.org>
2013-06-03 22:12:49 +0100

    make thread function into a proper function

--- examples/random_thread.lua
+++ examples/random_thread.lua
@@ -8,12 +8,27 @@ local thread_code = [[
     -- print thread's parameter.
     print("CHILD: received params:", ...)
 
-    require 'torch-env'
+    --require 'torch-env'
     require 'torch'
     torch.manualSeed(1)
     local mean = torch.randn(1000000):add(100):mean()
     return mean
 ]]
+
+function thread_func(...)
+    -- print thread's parameter.
+    print("CHILD: received params:", ...)
+
+    -- this somehow breaks things, is it pipe or torch.PipeFile?
+    --require 'torch-env'
+    require 'torch'
+    torch.manualSeed(1)
+    local mean = torch.randn(1000000):add(100):mean()
+    local ff = io.popen('ls','r')
+    local ss = ff:read('*l')
+    ff:close()
+    return mean,ss
+end
 
 require 'torch'
 
@@ -23,9 +38,9 @@ local expectedMean = torch.randn(1000000):add(100):mean()
 local nChildren = 10
 local children = {}
 for i = 1, nChildren do
-    local child = llthreads.new(thread_code, "number:", 1234, "nil:", nil, "bool:", true)
-    assert(child:start())
-    children[i] = child
+   local child = llthreads.new(string.dump(thread_func), "number:", 1234, "nil:", nil, "bool:", true)
+   assert(child:start())
+   children[i] = child
 end
 
 local results = {}
